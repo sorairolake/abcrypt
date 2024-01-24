abf55d2b224c2900e3fb27dcb00963af1ff00208
Shun Sakai <sorairolake@protonmail.ch>
2024-01-25 06:21:52 +0900

    Add Wasm bindings

---
+++ crates/wasm/README.md
@@ -0,0 +1,63 @@
+<!--
+SPDX-FileCopyrightText: 2022 Shun Sakai
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+-->
+
+# Wasm Bindings for abcrypt
+
+[![CI][ci-badge]][ci-url]
+[![Version][version-badge]][version-url]
+[![Docs][docs-badge]][docs-url]
+![License][license-badge]
+
+This crate ([`abcrypt-wasm`][version-url]) is the Wasm bindings for the
+[`abcrypt`] crate.
+
+## Usage
+
+### Build
+
+```sh
+wasm-pack build
+```
+
+### Documentation
+
+See the [documentation][docs-url] for more details.
+
+## Minimum supported Rust version
+
+The minimum supported Rust version (MSRV) of this library is v1.70.0.
+
+## Changelog
+
+Please see [CHANGELOG.adoc].
+
+## Contributing
+
+Please see [CONTRIBUTING.adoc].
+
+## License
+
+Copyright &copy; 2022&ndash;2024 Shun Sakai (see [AUTHORS.adoc])
+
+This library is distributed under the terms of either the _Apache License 2.0_
+or the _MIT License_.
+
+This project is compliant with version 3.0 of the [_REUSE Specification_]. See
+copyright notices of individual files for more details on copyright and
+licensing information.
+
+[ci-badge]: https://img.shields.io/github/actions/workflow/status/sorairolake/abcrypt/CI.yaml?branch=develop&label=CI&logo=github&style=for-the-badge
+[ci-url]: https://github.com/sorairolake/abcrypt/actions?query=branch%3Adevelop+workflow%3ACI++
+[version-badge]: https://img.shields.io/crates/v/abcrypt-wasm?style=for-the-badge
+[version-url]: https://crates.io/crates/abcrypt-wasm
+[docs-badge]: https://img.shields.io/docsrs/abcrypt-wasm?label=Docs.rs&logo=docsdotrs&style=for-the-badge
+[docs-url]: https://docs.rs/abcrypt-wasm
+[license-badge]: https://img.shields.io/crates/l/abcrypt-wasm?style=for-the-badge
+[`abcrypt`]: https://crates.io/crates/abcrypt
+[CHANGELOG.adoc]: CHANGELOG.adoc
+[CONTRIBUTING.adoc]: ../../CONTRIBUTING.adoc
+[AUTHORS.adoc]: ../../AUTHORS.adoc
+[_REUSE Specification_]: https://reuse.software/spec/
---
+++ crates/wasm/tests/decrypt.rs
@@ -0,0 +1,109 @@
+// SPDX-FileCopyrightText: 2022 Shun Sakai
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Lint levels of rustc.
+#![forbid(unsafe_code)]
+#![deny(missing_debug_implementations, missing_docs)]
+#![warn(rust_2018_idioms)]
+// Lint levels of Clippy.
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
+
+use wasm_bindgen::JsValue;
+use wasm_bindgen_test::wasm_bindgen_test;
+
+const PASSPHRASE: &[u8] = b"passphrase";
+const TEST_DATA: &[u8] = include_bytes!("data/data.txt");
+// Generated using `abcrypt` crate version 0.1.0.
+const TEST_DATA_ENC: &[u8] = include_bytes!("data/data.txt.abcrypt");
+
+#[wasm_bindgen_test]
+fn success() {
+    let plaintext = abcrypt_wasm::decrypt(TEST_DATA_ENC, PASSPHRASE)
+        .map_err(JsValue::from)
+        .unwrap();
+    assert_eq!(plaintext, TEST_DATA);
+}
+
+#[wasm_bindgen_test]
+fn incorrect_passphrase() {
+    let result = abcrypt_wasm::decrypt(TEST_DATA_ENC, b"password");
+    assert!(result.is_err());
+}
+
+#[wasm_bindgen_test]
+fn invalid_input_length() {
+    {
+        let data =
+            vec![u8::default(); (abcrypt_wasm::header_size() + abcrypt_wasm::tag_size()) - 1];
+        let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+        assert!(result.is_err());
+    }
+
+    {
+        let data = vec![u8::default(); abcrypt_wasm::header_size() + abcrypt_wasm::tag_size()];
+        let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+        assert!(result.is_err());
+    }
+}
+
+#[wasm_bindgen_test]
+fn invalid_magic_number() {
+    let mut data: [u8; TEST_DATA_ENC.len()] = TEST_DATA_ENC.try_into().unwrap();
+    data[0] = u32::from('b').try_into().unwrap();
+    let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+    assert!(result.is_err());
+}
+
+#[wasm_bindgen_test]
+fn unknown_version() {
+    let mut data: [u8; TEST_DATA_ENC.len()] = TEST_DATA_ENC.try_into().unwrap();
+    data[7] = 1;
+    let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+    assert!(result.is_err());
+}
+
+#[wasm_bindgen_test]
+fn invalid_params() {
+    let mut data: [u8; TEST_DATA_ENC.len()] = TEST_DATA_ENC.try_into().unwrap();
+
+    {
+        data[8..12].copy_from_slice(&u32::to_le_bytes(7));
+        let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+        assert!(result.is_err());
+    }
+
+    {
+        data[12..16].copy_from_slice(&u32::to_le_bytes(0));
+        let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+        assert!(result.is_err());
+    }
+
+    {
+        data[16..20].copy_from_slice(&u32::pow(2, 24).to_le_bytes());
+        let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+        assert!(result.is_err());
+    }
+}
+
+#[wasm_bindgen_test]
+fn invalid_header_mac() {
+    let mut data: [u8; TEST_DATA_ENC.len()] = TEST_DATA_ENC.try_into().unwrap();
+    let mut header_mac: [u8; 64] = data[76..140].try_into().unwrap();
+    header_mac.reverse();
+    data[76..140].copy_from_slice(&header_mac);
+    let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+    assert!(result.is_err());
+}
+
+#[wasm_bindgen_test]
+fn invalid_mac() {
+    let data: [u8; TEST_DATA_ENC.len()] = TEST_DATA_ENC.try_into().unwrap();
+    let start_mac = data.len() - abcrypt_wasm::tag_size();
+    let mut data = data;
+    let mut mac = data[start_mac..].to_vec();
+    mac.reverse();
+    data[start_mac..].copy_from_slice(&mac);
+    let result = abcrypt_wasm::decrypt(&data, PASSPHRASE);
+    assert!(result.is_err());
+}
