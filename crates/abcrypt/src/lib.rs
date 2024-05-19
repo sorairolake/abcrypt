14f85691bf1cdf999abc5c0199a39c35359abe08
Shun Sakai <sorairolake@protonmail.ch>
2024-05-19 17:09:46 +0900

    Rename `doc_cfg` to `docsrs`
    Fix the `unexpected_cfgs` lint warning.

--- crates/abcrypt/src/lib.rs
+++ crates/abcrypt/src/lib.rs
@@ -87,7 +87,7 @@
 
 #![doc(html_root_url = "https://docs.rs/abcrypt/0.3.4/")]
 #![no_std]
-#![cfg_attr(doc_cfg, feature(doc_auto_cfg, doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
 // Lint levels of rustc.
 #![forbid(unsafe_code)]
 #![deny(missing_debug_implementations, missing_docs)]
