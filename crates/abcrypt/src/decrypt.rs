f0bdf5e9bbb87ce3504d1bb6cc08f37eedc467d7
Shun Sakai <sorairolake@protonmail.ch>
2026-08-31 05:54:53 +0900

    Fix commented assertions in decrypt.rs

--- crates/abcrypt/src/decrypt.rs
+++ crates/abcrypt/src/decrypt.rs
@@ -119,7 +119,7 @@ impl<'c> Decryptor<'c> {
     /// let cipher = Decryptor::new(&ciphertext, passphrase).unwrap();
     /// let mut buf = [u8::default(); 14];
     /// cipher.decrypt(&mut buf).unwrap();
-    /// # assert_eq!(buf, *data);
+    /// assert_eq!(buf, *data);
     /// ```
     pub fn decrypt<B: AsMut<[u8]> + ?Sized>(self, buf: &mut B) -> Result<()> {
         let inner = |decryptor: Self, buf: &mut [u8]| -> Result<()> {
@@ -155,7 +155,7 @@ impl<'c> Decryptor<'c> {
     ///
     /// let cipher = Decryptor::new(&ciphertext, passphrase).unwrap();
     /// let plaintext = cipher.decrypt_to_vec().unwrap();
-    /// # assert_eq!(plaintext, data);
+    /// assert_eq!(plaintext, data);
     /// ```
     #[cfg(feature = "alloc")]
     pub fn decrypt_to_vec(self) -> Result<Vec<u8>> {
@@ -211,7 +211,7 @@ impl<'c> Decryptor<'c> {
 /// let passphrase = "passphrase";
 ///
 /// let plaintext = abcrypt::decrypt(ciphertext, passphrase).unwrap();
-/// # assert_eq!(plaintext, data);
+/// assert_eq!(plaintext, data);
 /// ```
 #[cfg(feature = "alloc")]
 pub fn decrypt(ciphertext: impl AsRef<[u8]>, passphrase: impl AsRef<[u8]>) -> Result<Vec<u8>> {
